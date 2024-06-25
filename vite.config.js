448c306352960d6f1c3124b666219d4baab357c4
jatin34090 <jatin34090@gmail.com>
2024-06-26 00:00:46 +0530

    solve proxy errors

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-     '/api': 'https://teacher-allotment-system-backend.onrender.com',
+     '/api': 'https://teacher-allotment-system-backend.onrender.com/',
           // '/api': 'http://localhost:5000/',
 
     }

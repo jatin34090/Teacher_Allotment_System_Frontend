b2798d428fcd6afded2f00e0107aca7baa74b832
jatin34090 <jatin34090@gmail.com>
2024-06-25 23:23:25 +0530

    Update vite config file

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-    //  '/api': 'https://teacher-allotment-system-backend.onrender.com/',
-          '/api': 'http://localhost:5000/',
+     '/api': 'https://teacher-allotment-system-backend.onrender.com/',
+          // '/api': 'http://localhost:5000/',
 
     }
   },
